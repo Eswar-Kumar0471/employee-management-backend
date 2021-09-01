daac59efe4076a7d2a85a597551d93537dac987c
Eswar-Kumar0471 <eswarkumar.boina@gmail.com>
2021-09-01 10:46:21 +0530

    backend setup

---
+++ employee-management-springboot/employee-management-springboot/src/main/java/tech/ek/employeemanagementspringboot/EmployeeManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package tech.ek.employeemanagementspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSpringbootApplication.class, args);
+	}
+
+}
---
+++ employee-management-springboot/employee-management-springboot/src/test/java/tech/ek/employeemanagementspringboot/EmployeeManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ek.employeemanagementspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
